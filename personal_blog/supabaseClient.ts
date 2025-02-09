e2a51ced08bf8570539cab9e774c116e7d7ee14c
Joshua Sevy <joshuasevy@outlook.com>
2025-02-09 00:40:37 -0700

    Fix delpoy issues

--- personal_blog/supabaseClient.ts
+++ personal_blog/supabaseClient.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 const env = config();
 
-const SUPABASE_URL = env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = env.SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL || (() => { throw new Error("SUPABASE_URL is not defined") })();
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || (() => { throw new Error("SUPABASE_ANON_KEY is not defined") })();
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
